084f00bcf1ed4cd980cbd6e6091c0639dad4a416
da-martin <martindavidandreas@gmail.com>
2022-03-13 18:29:39 +0100

    Added first button functionality

--- generadores.js
+++ generadores.js
@@ -1,9 +1,11 @@
-const PROBABLE = 0;
-const CINCUENTA = 1;
-const IMPROBABLE = 2;
+const LIKELY = 0;
+const FIFTY = 1;
+const UNLIKELY = 2;
+const DESCRIPTION = 3;
+
 
 const tables = [
-  ["probable","Los asuntos son como piensas, y de manera excepcional",
+  ["Respuesta sí o no probable","Los asuntos son como piensas, y de manera excepcional",
   "Es lo que tú piensas, pero quizás no de la forma que esperabas",
   "Exactamente lo que tú crees",
   "Los asuntos son como piensas, y de manera excepcional",
@@ -13,14 +15,14 @@ const tables = [
   "Tus expectativas no se van a cumplir, aunque quizás puedas sacar algo bueno de todo esto",
   "Lo siento pero las cosas no son como tú pensabas",
   "Te advierto que la situación va ser lo contrario de lo que esperabas"],
-  ["cincuenta","Los asuntos son como piensas, y de manera excepcional",
+  ["Respuesta sí o no cincuenta cincuenta","Los asuntos son como piensas, y de manera excepcional",
   "Es lo que tú piensas, pero quizás no de la forma que esperabas",
   "Exactamente lo que tú crees",
   "En realidad las cosas sólo son parcialmente como tú querrías",
   "Tus expectativas no se van a cumplir, aunque quizás puedas sacar algo bueno de todo esto",
   "Lo siento pero las cosas no son como tú pensabas",
   "Te advierto que la situación va ser lo contrario de lo que esperabas"],
-  ["improbable","Los asuntos son como piensas, y de manera excepcional",
+  ["Respuesta sí o no improbable","Los asuntos son como piensas, y de manera excepcional",
   "Es lo que tú piensas, pero quizás no de la forma que esperabas",
   "Exactamente lo que tú crees",
   "En realidad las cosas sólo son parcialmente como tú querrías",
@@ -29,7 +31,18 @@ const tables = [
   "Te advierto que la situación va ser lo contrario de lo que esperabas",
   "Tus expectativas no se van a cumplir, aunque quizás puedas sacar algo bueno de todo esto",
   "Lo siento pero las cosas no son como tú pensabas",
-  "Te advierto que la situación va ser lo contrario de lo que esperabas"]
+  "Te advierto que la situación va ser lo contrario de lo que esperabas"],
+  ["Descripción","Relacionado con personaje protagonista",
+  "Relacionado con un personaje secundario",
+  "Relacionado con alguna organización",
+  "Lo opuesto a un personaje secundario",
+  "Relacionado con un objetivo",
+  "Alejado de un objetivo",
+  "Inspira alegría",
+  "Inspira tristeza",
+  "Inspira temor",
+  "Inspira asco",
+  "Inspira confianza"]
 ];
 // Los elementos del array con el formato .5 tienen que volver a 
 //  llamar a rollOnTable en la tabla indicada.
@@ -38,6 +51,34 @@ const tables = [
 
 //Tira en la tabla indicada por el parámetro
 function rollOnTable(tableNumber){
-
-  
+  let randomIndex = Math.floor(Math.random() * (tables[tableNumber].length - 1)) + 1;
+  let randomResult = tables[tableNumber][randomIndex];
+  //console.log(tables[tableNumber]);
+  console.log(randomIndex + "/" + tables[tableNumber].length);
+  return randomResult;
 }
+
+const btnLikely = document.querySelector('#likely');
+const btnFifty = document.querySelector('#fifty');
+const btnUnlikely = document.querySelector('#unlikely');
+const btnDescription = document.querySelector('#description');
+
+btnLikely.addEventListener("click", function() {
+  const txtResults = document.querySelector('#results');
+  txtResults.textContent = rollOnTable(LIKELY);
+});
+
+btnFifty.addEventListener("click", function() {
+  const txtResults = document.querySelector('#results');
+  txtResults.textContent = rollOnTable(FIFTY);
+});
+
+btnUnlikely.addEventListener("click", function() {
+  const txtResults = document.querySelector('#results');
+  txtResults.textContent = rollOnTable(UNLIKELY);
+});
+
+btnDescription.addEventListener("click", function() {
+  const txtResults = document.querySelector('#results');
+  txtResults.textContent = rollOnTable(DESCRIPTION);
+});
